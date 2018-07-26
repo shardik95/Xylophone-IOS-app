cd11f81d099c163e3e7215a41daa7369f9749b52
shardik95 <shardik95@ccs.neu.edu>
2018-07-26 01:29:37 -0400

    Multiple functions and array

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -5,14 +5,21 @@ import AVFoundation
 class ViewController: UIViewController, AVAudioPlayerDelegate{
     
     var audioPlayer : AVAudioPlayer!
+    var soundFile:String = "";
+    let soundFileArray = ["note1","note2","note3","note4","note5","note6","note7"];
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func notePressed(_ sender: UIButton) {
+        soundFile = soundFileArray[sender.tag-1];
+        playAudio();
+    }
+    
+    func playAudio(){
         
-        let soundURL = Bundle.main.url(forResource: "note\(sender.tag)", withExtension: "wav");
+        let soundURL = Bundle.main.url(forResource: soundFile , withExtension: "wav");
         
         do{
             try audioPlayer = AVAudioPlayer(contentsOf: soundURL!);
